50d3bd684508eef12dece27b0230c109b805d59c
Samer Ziade <21907+samerziade@users.noreply.github.com>
2018-12-23 13:27:24 -0500

    Improving error handling in main app loop

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,24 @@ import { config } from './Config'
 import { Log } from './Log'
 
 const { count, interval } = config.application
-let counter = 0
+let processCounter = 0
+let errorCounter = 0
 
-const timer = setInterval(() => {
-  send(config).catch(err => Log.error(err))
+const timer = setInterval(async () => {
+  processCounter++
 
-  if (count > 0 && count === ++counter) {
+  if (errorCounter >= 19) {
+    Log.error(`${errorCounter} errors were encounted. Exiting.`)
     clearInterval(timer)
+  } else if (count > 0 && count === processCounter) {
+    clearInterval(timer)
+  }
+
+  try {
+    await send(config)
+    errorCounter = 0
+  } catch (err) {
+    errorCounter++
+    Log.error(err)
   }
 }, interval)
